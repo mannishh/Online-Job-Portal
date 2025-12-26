3ba149cb77a4ee6c6df56b75aae6bed1298787b5
Manish Hengaju <manish.hju@gmail.com>
2025-12-26 19:52:59 +0545

    dashboard layout created

--- frontend/job-portal/src/pages/Employer/EmployerDashboard.jsx
+++ frontend/job-portal/src/pages/Employer/EmployerDashboard.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import {
+  Plus,
+  Briefcase,
+  Users,
+  Building2,
+  TrendingUp,
+  CheckCircle2,
+} from "lucide-react";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import DashboardLayout from "../../components/layout/DashboardLayout";
 
 const EmployerDashboard = () => {
-  return (
-    <div>
-      empolo
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default EmployerDashboard
+  const [dashboardData, setDashboardData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getDashboardOverView = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axiosInstance.get(API_PATHS.DASHBOARD.OVERVIEW);
+      if (response.status === 200) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getDashboardOverView();
+    return () => {};
+  }, []);
+
+  return (
+  <DashboardLayout activeMenu='employer-dashboard'>
+    EmployerDashboard
+    </DashboardLayout>
+    );
+};
+
+export default EmployerDashboard;
